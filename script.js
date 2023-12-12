9c5322b5afe51228a39f3b6e057ffa7d0bfe8eff
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-12 20:40:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	// Check for empty strings
+  if (s1 === '' || s2 === '') {
+    return -1;
+  }
+
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerStr = s1.toLowerCase();
+  const lowerSubStr = s2.toLowerCase();
+
+  // Find the index of the substring
+  const index = lowerStr.indexOf(lowerSubStr);
+
+  return index;
+	
 }
 
 // Please do not change the code below
